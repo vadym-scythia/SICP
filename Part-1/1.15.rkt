16b1bb20bec64a115f5a3b2edd2735748a118ac5
Vadym Mandrichenko <vmand@softserveinc.com>
2021-04-26 22:56:35 +0300

    1.15

---
+++ Part-1/1.15.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+; a. 5 times
+; b. teta(log(a))
+
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
