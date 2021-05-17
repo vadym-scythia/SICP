6116a5361f27ca877a2c8807a4049277c7d14498
Vadym Mandrichenko <vmand@softserveinc.com>
2021-05-17 10:25:21 +0300

    Finish 1.16 practice task

---
+++ Part-1/1.16.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (fast-expt b n)
+  (fast-expt-iter b n 1))
+
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* b a)))))
+
+(define (square a)
+  (* a a))
+
+(define (even? n)
+  (= (remainder n 2) 0))
