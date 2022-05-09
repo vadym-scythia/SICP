70ab555f782541b06a1aa9cd563d75fc85e6f483
VadymMan <vadymmandrichenko@gmail.com>
2022-05-09 22:41:43 +0300

    Add new methods to find sqrt.
    Added average-damp and Newton's method, using functions as arguments and returning values.
    Added comment to 1.4 exercise.

---
+++ Test/NewtonsMethod.rkt
@@ -0,0 +1,34 @@
+#lang sicp
+
+(define (square x)
+  (* x x))
+
+(define tolerance 0.0000000000000000000000000000000000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define dx 0.000001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
