28c2dccb12bee5ee07df44df69a395e061625963
VadymMan <vadymmandrichenko@gmail.com>
2022-05-21 23:37:51 +0300

    Add rational-arithmetic

---
+++ Test/rational-arithmetic.rkt
@@ -0,0 +1,50 @@
+#lang sicp
+
+; need
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+; rational numbers
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+; operations with rational numbers
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; util
+
+(define (print-rat x)
+  ;(newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
