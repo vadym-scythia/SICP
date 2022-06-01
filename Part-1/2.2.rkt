32bef2776e3a9fcb6d874cb553bc40f67fb219e3
VadymMan <vadymmandrichenko@gmail.com>
2022-06-01 22:28:32 +0300

    Add 2.2

---
+++ Part-1/2.2.rkt
@@ -0,0 +1,45 @@
+#lang sicp
+
+; point
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+; segment
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+; midpoint
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment))
+                    (x-point (end-segment segment)))
+                 2)
+              (/ (+ (y-point (start-segment segment))
+                    (y-point (end-segment segment)))
+                 2)))
+
+(define (test x1 y1 x2 y2)
+  (define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ",")
+    (display (y-point p))
+    (display ")"))
+  (print-point (midpoint-segment (make-segment (make-point x1 y1)
+                                  (make-point x2 y2)))))
+                
