ddf75c4f19cef958c0773b7c50ec809aa256163f
VadymMan <vadymmandrichenko@gmail.com>
2022-06-16 23:48:20 +0300

    Update interval-arithmetic

--- Test/interval-arithmetic.rkt
+++ Test/interval-arithmetic.rkt
@@ -28,3 +28,12 @@
         (mul-interval x
                       (make-interval (/ 1.0 (upper-bound y))
                                      (/ 1.0 (lower-bound y)))))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
