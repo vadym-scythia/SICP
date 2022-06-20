ec931637824f9a6adcec4ea64ab56e686d4b44d4
VadymMan <vadymmandrichenko@gmail.com>
2022-06-20 22:08:09 +0300

    Add 2.12
    I don't know why DrRacket adding/deleting files like 2.1 in this commit, but let it be.

---
+++ Part-1/2.12.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+; util
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (cdr interval))
+(define (lower-bound interval) (car interval))
+
+(define (width interval)
+  (/ (+ (lower-bound interval)
+        (upper-bound interval))
+     2))
+
+(define (make-center-percent center percent)
+  (let ((x (/ (* percent center) 100)))
+    (make-interval (- center x) (+ center x))))
+
+(define (center interval)
+(/ (+ (lower-bound interval) (upper-bound interval)) 2))
+
+(define (percent interval)
+  (let ((center (/ (+ (upper-bound interval) (lower-bound interval)) 2))
+        (tolerance (- (/ (+ (upper-bound interval) (lower-bound interval)) 2) (lower-bound interval))))
+    (/ (* 100 tolerance) center)))
