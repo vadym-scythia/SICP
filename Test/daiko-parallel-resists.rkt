4fc6bf81bea4120e98071364a29beb16e23dde22
VadymMan <vadymmandrichenko@gmail.com>
2022-06-21 22:28:05 +0300

    Add daiko parallel resists

---
+++ Test/daiko-parallel-resists.rkt
@@ -0,0 +1,44 @@
+#lang sicp
+
+; util
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (cdr interval))
+(define (lower-bound interval) (car interval))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (let ((upper-y (upper-bound y))
+        (lower-y (lower-bound y)))
+    (if (or (= upper-y 0) (= lower-y 0))
+        "ERROR: interval's bounds shouldn't be 0!"
+        (mul-interval x
+                      (make-interval (/ 1.0 (upper-bound y))
+                                     (/ 1.0 (lower-bound y)))))))
+
+; main
+
+(define (par1 r1 r2)
+    (div-interval (mul-interval r1 r2)
+                  (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+    (let ((one (make-interval 1 1)))
+        (div-interval one
+                      (add-interval (div-interval one r1)
+                                    (div-interval one r1)))))
+                                    
+; test
+
+;(define test ((par1 (make-interval 1 2) (make-interval 2 3))))
