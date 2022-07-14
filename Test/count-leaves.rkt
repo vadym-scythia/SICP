ddd067e828e938a94bc9f5170046dbca992c18de
VadymMan <vadymmandrichenko@gmail.com>
2022-07-14 22:03:57 +0300

    Add count-leaves, 2.24

---
+++ Test/count-leaves.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1(length (cdr items)))))
+
+(define x (cons (list 1 2) (list 3 4)))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
