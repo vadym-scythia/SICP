31bd6f4b04cd54949356ed7d97ac8a814267cde9
VadymMan <vadymmandrichenko@gmail.com>
2022-07-14 22:16:02 +0300

    Add 2.25

---
+++ Exercises/2.25.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+
+; (1 3 (5 7) 9)
+(define test1 (car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9)))))))
+
+; ((7))
+(define test2 (car (car (list (list 7)))))
+
+; (1 (2 (3 (4 (5 (6 7))))))
+(define test3 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))))))))))))))
