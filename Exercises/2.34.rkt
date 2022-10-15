d63bc39d7c0e87004f4b9f7862ee7323ca5a4114
VadymMan <vadymmandrichenko@gmail.com>
2022-10-15 21:18:12 +0300

    WIP:  2.34, add  experiments

---
+++ Exercises/2.34.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) )
+              0
+              coefficient-sequence))
