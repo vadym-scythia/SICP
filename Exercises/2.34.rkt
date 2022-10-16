615febe3d151b5d22da15f9fd4efb4311f0f0b65
VadymMan <vadymmandrichenko@gmail.com>
2022-10-16 13:22:50 +0300

    Add 2.34, drop experiments

--- Exercises/2.34.rkt
+++ Exercises/2.34.rkt
@@ -7,6 +7,6 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (horner-eval x coefficient-sequence)
-  (accumulate (lambda (this-coeff higher-terms) )
+  (accumulate (lambda (this-coeff higher-terms) (+ (* higher-terms x) this-coeff))
               0
               coefficient-sequence))
