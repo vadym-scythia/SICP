4503cf6bc036f3ce7457b22ee99ae5bdebf7f4c4
VadymMan <vadymmandrichenko@gmail.com>
2022-11-23 11:53:49 +0200

    Add 2.39

---
+++ Exercises/2.39.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))  
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (revers-left sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+(reverse (list 1 2 3))
+(revers-left (list 1 2 3))
+
+;(append (list nil 1 2))
