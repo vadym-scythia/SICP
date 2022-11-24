3d48119de1c7a70d3bb3dde85ef95407f9e7f952
VadymMan <vadymmandrichenko@gmail.com>
2022-11-24 15:02:37 +0200

    Add flatmap part-2, experiments

--- Test/flat-map.rkt
+++ Test/flat-map.rkt
@@ -1,5 +1,7 @@
 #lang sicp
 
+; util
+
 (define (enumerate-interval low high)
   (if (> low high)
       (list)
@@ -30,12 +32,27 @@
 (define (prime? n)
   (= n (smallest-divisor n)))
 
+; flatmap
+
 (define n 3)
 
-(accumulate append
-            nil
-            (map (lambda (i)
-                   (filter (lambda (j)
-                             (if (prime? (+ i j)) (list i j)))
-                           (enumerate-interval 1 i)))
-                 (enumerate-interval 1 n)))
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cdr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
+(prime-sum-pairs n)
+               
