654041e3ee99e0f73b2401e2edc2602e574b16c2
VadymMan <vadymmandrichenko@gmail.com>
2022-11-25 21:21:12 +0200

    Add 2.40 part-2

--- Exercises/2.40.rkt
+++ Exercises/2.40.rkt
@@ -21,6 +21,24 @@
 (define (flatmap proc seq)
   (accumulate append nil (map proc seq)))
 
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (* test-divisor test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cdr pair) (+ (car pair) (cadr pair))))
+
 ; result
 
 (define n 5)
@@ -35,3 +53,10 @@
                    (enumerate-interval 1 n))))
 
 (unique-pairs n)
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
+
+(prime-sum-pairs n)
