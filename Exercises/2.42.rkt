f82bf042079e3daf9d159de930dea111ebdc7eb8
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-01-28 23:32:55 +0200

    wip

--- Exercises/2.42.rkt
+++ Exercises/2.42.rkt
@@ -1,23 +1,36 @@
-#lang scheme
+#lang sicp
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      (list)
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define empty-board `())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (list new-row k) rest-of-queens))
 
 (define (queens board-size)
   (define (queen-cols k)
     (if (= k 0)
         (list empty-board)
-        (filter
-         (lambda (positions) (safe? k position))
          (flatmap
           (lambda (rest-of-queens)
             (map (lambda (new-row)
                    (adjoin-position new-row k rest-of-queens))
                  (enumerate-interval 1 board-size)))
-          (queen-cols (- k 1))))))
+          (queen-cols (- k 1)))))
   (queen-cols board-size))
 
-(define empty-board `())
+(queens 2)
 
-(define (adjoin-position new-row k rest-of-queens)
-  
-
-;(define (safe k positions)
-  
+; add safe?
